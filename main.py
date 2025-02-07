2d00d7aab06ab411705d0c8e88e7aae5ea5cb9ba
dipak-soni <dipaksoni2832@gmail.com>
2025-02-07 17:49:17 +0530

    added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # uvicorn main:app --reload
 # main= module name
 # app= FastAPI instance name
+# http://localhost:8000/docs to enter the swagger UI api testing 
+# http://localhost:8000/redoc to enter the redoc UI api testing
 
 from fastapi import FastAPI
 app=FastAPI()
@@ -13,3 +15,15 @@ def index():
 @app.get("/about")   # function name does not matter
 def index():
     return {"message":"About Page"}
+
+# dynamic taking data from path
+@app.get("/blog/{id}")
+def index(id):
+    # fetch blog with id ==
+    return {"data":id}
+
+
+@app.get("/blog/{id}/comments")
+def index(id:int):
+    # fetch comments with id
+    return {"comments":"This is a comment section"}
