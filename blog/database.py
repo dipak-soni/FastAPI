0aebc6ce09ef0630413110b9f7de3b0dc4d2fd2f
dipak-soni <dipaksoni2832@gmail.com>
2025-02-07 19:43:19 +0530

    added

---
+++ blog/database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+engine = create_engine("sqlite://blog.db", connect_args={"check_same_thread": False})
+
